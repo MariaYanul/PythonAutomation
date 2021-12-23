edded10b7977b178c98e7d579333dc660530aa00
yanulmaria <yanulmaria@gmail.com>
2021-12-23 20:20:36 +0200

    hw14

---
+++ src/hw14/zoo.py
@@ -0,0 +1,11 @@
+class Zoo:
+    def __init__(self, animal_type: str, animal_name: str, num_cage: int) -> None:
+        self.animal_type = animal_type
+        self.animal_name = animal_name
+        self.num_cage = num_cage
+
+if __name__ == "__main__":
+    animal = Zoo('mammals', 'Grizly', 13)
+    print(animal.animal_type)
+    print(animal.animal_name)
+    print(animal.num_cage)
